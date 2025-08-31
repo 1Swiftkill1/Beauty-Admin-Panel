6b8373ec1b547c68bcf8d0a6f08606bc6f1010db
1Swiftkill1 <Abesin4ik@mail.ru>
2025-08-31 20:01:35 +0300

    Initial commit: project setup with React, TS, useHttp hook, Context API, reducer, and dayjs integration

---
+++ src/context/appointments/reducer.ts
@@ -0,0 +1,27 @@
+import { type AppointmentAction, ActionsTypes } from "./actions";
+import type { IAppointment, ActiveAppointment } from "../../shared/interfaces/appointment.interface";
+import type { LoadingStatusOptions } from "../../hooks/http.hook";
+
+export interface IAppointmentState {
+    allAppointments: IAppointment[] | [];
+    activeAppointments: ActiveAppointment[] | [];
+    appointmentLoadingStatus: LoadingStatusOptions;
+}
+
+export default function reducer(
+    state: IAppointmentState,
+    action: AppointmentAction
+): IAppointmentState {
+    switch (action.type) {
+        case ActionsTypes.SET_ALL_APPOINTMENTS:
+            return { ...state, allAppointments: action.payload, appointmentLoadingStatus: 'idle' };
+        case ActionsTypes.SET_ACTIVE_APPOINTMENTS:
+            return { ...state, activeAppointments: action.payload, appointmentLoadingStatus: 'idle' };
+        case ActionsTypes.FETCHING_APPOINTMENTS:
+            return { ...state, appointmentLoadingStatus: 'loading' };
+        case ActionsTypes.ERROR_FETCHING_APPOINTMENTS:
+            return { ...state, appointmentLoadingStatus: 'error' };
+        default:
+            return state;
+    }
+}
